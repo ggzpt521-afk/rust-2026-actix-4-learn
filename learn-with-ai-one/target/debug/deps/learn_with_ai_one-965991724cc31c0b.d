f6a29e25158923c244b033c6834a06592de6b7a2
zhaopengtao <zhaopengtao001@51talk.com>
2026-01-12 10:38:21 +0800

    删除

--- learn-with-ai-one/target/debug/deps/learn_with_ai_one-965991724cc31c0b.d
+++
@@ -1,8 +0,0 @@
-/Users/zpt/AnewProject/rust/learn-one/learn-with-ai-one/target/debug/deps/learn_with_ai_one-965991724cc31c0b.d: src/main.rs src/13_lifetimes.rs src/14_std_lib_macros.rs src/15_async_await.rs
-
-/Users/zpt/AnewProject/rust/learn-one/learn-with-ai-one/target/debug/deps/learn_with_ai_one-965991724cc31c0b: src/main.rs src/13_lifetimes.rs src/14_std_lib_macros.rs src/15_async_await.rs
-
-src/main.rs:
-src/13_lifetimes.rs:
-src/14_std_lib_macros.rs:
-src/15_async_await.rs:
--- learn-with-ai-one/target/debug/deps/learn_with_ai_one-c8fafe6833b19710.d
+++
@@ -1,8 +0,0 @@
-/Users/zpt/AnewProject/rust/learn-one/learn-with-ai-one/target/debug/deps/learn_with_ai_one-c8fafe6833b19710.d: src/main.rs src/13_lifetimes.rs src/14_std_lib_macros.rs src/15_async_await.rs
-
-/Users/zpt/AnewProject/rust/learn-one/learn-with-ai-one/target/debug/deps/liblearn_with_ai_one-c8fafe6833b19710.rmeta: src/main.rs src/13_lifetimes.rs src/14_std_lib_macros.rs src/15_async_await.rs
-
-src/main.rs:
-src/13_lifetimes.rs:
-src/14_std_lib_macros.rs:
-src/15_async_await.rs:
